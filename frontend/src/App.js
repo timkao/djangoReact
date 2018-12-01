e7bad6d3b6c63f8ca32c51cc61cc3cb387e8045a
Tim Kao <pakuya.kao@gmail.com>
2018-12-01 15:36:32 -0500

    backend code to django

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { list: [] }
+  }
+
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/api/')
+    .then(res => {
+      console.log(res.data)
+      this.setState({list: res.data})
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.state.list.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <span>{item.body}</span>
+          </div>
+        ))}
       </div>
     );
   }
